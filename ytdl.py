13f3ec0e734531e1b0c574e3726223a5fcd2a1ac
colons <colons@colons.co>
2021-04-13 12:02:46 +0100

    Use safari to play videos and only ever request MP4s.

--- ytdl.py
+++ ytdl.py
@@ -2,7 +2,7 @@
 
 '''
 Directions:
-    
+
     - install youtube-dl via Pip (e.g. using the StaSh command: https://github.com/ywangd/stash)
     - add this script as a Share extension through Settings -> Share Extension Shortcuts
     - while watching a video in the YouTube site or app, just share the video to Pythonista and select this script
@@ -19,41 +19,47 @@ import console
 import clipboard
 import os
 import sys
+from objc_util import UIApplication, nsurl
+from urllib.parse import urlencode
 
 outdir = os.path.expanduser("~/Documents/Downloads")
 try:
-	os.mkdir(outdir)
+    os.mkdir(outdir)
 except FileExistsError:
-	pass
+    pass
 
 if appex.get_attachments():
-	# e.g. share from YouTube app
-	url = appex.get_attachments()[0]
+    # e.g. share from YouTube app
+    url = appex.get_attachments()[0]
 elif appex.get_urls():
-	# e.g. share from Safari
-	url = appex.get_urls()[0]
+    # e.g. share from Safari
+    url = appex.get_urls()[0]
 elif appex.get_text():
-	url = appex.get_text()
+    url = appex.get_text()
 elif clipboard.get():
-	url = clipboard.get()
+    url = clipboard.get()
 
 print("URL: ", url)
 if not url or not url.startswith("http"):
-	url = input("No URL found - enter URL to download: ")
+    url = input("No URL found - enter URL to download: ")
 
 ydl_opts = {'outtmpl': os.path.join(outdir, '%(title)s.%(ext)s')}
 
-if sys.argv[1:] == ['--stream']:
-	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-		info = ydl.extract_info(url, download=False)
-		from objc_util import UIApplication, nsurl
-		from urllib.parse import urlencode
-		app = UIApplication.sharedApplication()
-		params = urlencode({'url': info['formats'][-1]['url']})
-		app.openURL_(nsurl('vlc-x-callback://x-callback-url/stream?' + params))
-else:
-	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-		info = ydl.extract_info(url, download=True)
-		filepath = ydl.prepare_filename(info)
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    selector = ydl.build_format_selector('best[ext=mp4]')
 
-	console.open_in(filepath)
+    info = ydl.extract_info(url, download=False)
+    if info.get('_type') == 'playlist':
+        raise NotImplementedError(f'{url} is a playlist')
+
+    info['selected_format'], = selector({'formats': item['formats']})
+
+    if sys.argv[1:] == ['--stream']:
+        app = UIApplication.sharedApplication()
+        params = {'url': info['formats'][-1]['url']}
+        # app.openURL_(nsurl('vlc-x-callback://x-callback-url/stream?' + urlencode(params)))
+        app.openURL_(nsurl(params['url'])
+    else:
+        filepath = ydl.prepare_filename(info)
+
+        console.open_in(filepath)
