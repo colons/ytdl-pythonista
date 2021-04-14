e1bbf23f374c56fac01c009544f52342cbd7ad62
colons <colons@colons.co>
2021-04-14 11:56:59 +0100

    Wrap docs, and get URLs first.

--- ytdl.py
+++ ytdl.py
@@ -3,14 +3,17 @@
 '''
 Directions:
 
-    - install youtube-dl via Pip (e.g. using the StaSh command: https://github.com/ywangd/stash)
-    - add this script as a Share extension through Settings -> Share Extension Shortcuts
-    - while watching a video in the YouTube site or app, just share the video to Pythonista and select this script
-    - the video will download, and when it's done you can share the video file itself with any app (e.g. VLC)
-Advanced usage:
-    - if you specify --stream as the script argument, this script will just grab the actual video URL and redirect you
-    to VLC, which will stream the video (without interruptions or ads!)
-'''
+    - install youtube-dl via Pip (e.g. using the StaSh command:
+      https://github.com/ywangd/stash)
+    - add this script as a Share extension through Settings -> Share Extension
+      Shortcuts
+    - while watching a video in the YouTube site or app, just share the video
+      to Pythonista and select this script
+    - the video will download, and when it's done you can share the video file
+      itself with any app (e.g. VLC) Advanced usage:
+    - if you specify --stream as the script argument, this script will just
+    grab the actual video URL and redirect you to VLC, which will stream the
+    video (without interruptions or ads!) '''
 
 from __future__ import unicode_literals
 import youtube_dl
@@ -20,7 +23,7 @@ import clipboard
 import os
 import sys
 from objc_util import UIApplication, nsurl
-from urllib.parse import urlencode
+# from urllib.parse import urlencode
 
 outdir = os.path.expanduser("~/Documents/Downloads")
 try:
@@ -28,12 +31,10 @@ try:
 except FileExistsError:
     pass
 
-if appex.get_attachments():
-    # e.g. share from YouTube app
-    url = appex.get_attachments()[0]
-elif appex.get_urls():
-    # e.g. share from Safari
+if appex.get_urls():
     url = appex.get_urls()[0]
+elif appex.get_attachments():
+    url = appex.get_attachments()[0]
 elif appex.get_text():
     url = appex.get_text()
 elif clipboard.get():
