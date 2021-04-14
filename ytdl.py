1176f44edc37d1c5489f4ccda2f5695ef8866a53
colons <colons@colons.co>
2021-04-14 12:05:20 +0100

    De-indent the docs, and work with shares from the Twitch app.

--- ytdl.py
+++ ytdl.py
@@ -3,27 +3,27 @@
 '''
 Directions:
 
-    - install youtube-dl via Pip (e.g. using the StaSh command:
-      https://github.com/ywangd/stash)
-    - add this script as a Share extension through Settings -> Share Extension
-      Shortcuts
-    - while watching a video in the YouTube site or app, just share the video
-      to Pythonista and select this script
-    - the video will download, and when it's done you can share the video file
-      itself with any app (e.g. VLC) Advanced usage:
-    - if you specify --stream as the script argument, this script will just
-    grab the actual video URL and redirect you to VLC, which will stream the
-    video (without interruptions or ads!) '''
+- install youtube-dl via Pip (e.g. using the StaSh command:
+  https://github.com/ywangd/stash)
+- add this script as a Share extension through Settings -> Share Extension
+  Shortcuts
+- while watching a video in the YouTube site or app, just share the video to
+  Pythonista and select this script
+- the video will download, and when it's done you can share the video file
+  itself with any app (e.g. VLC) Advanced usage:
+- if you specify --stream as the script argument, this script will just grab
+  the actual video URL and redirect you to VLC, which will stream the video
+  (without interruptions or ads!)
+
+'''
 
 from __future__ import unicode_literals
 import youtube_dl
 import appex
 import console
-import clipboard
 import os
 import sys
 from objc_util import UIApplication, nsurl
-# from urllib.parse import urlencode
 
 outdir = os.path.expanduser("~/Documents/Downloads")
 try:
@@ -31,16 +31,15 @@ try:
 except FileExistsError:
     pass
 
-if appex.get_urls():
-    url = appex.get_urls()[0]
-elif appex.get_attachments():
-    url = appex.get_attachments()[0]
-elif appex.get_text():
-    url = appex.get_text()
-elif clipboard.get():
-    url = clipboard.get()
+url = appex.get_url()
+
+if url is None:
+    text = appex.get_text()
+    if 'http' in text:
+        url = 'http' + text.split('http', 1)[1]
 
 print("URL: ", url)
+
 if not url or not url.startswith("http"):
     url = input("No URL found - enter URL to download: ")
 
@@ -58,7 +57,6 @@ with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     if sys.argv[1:] == ['--stream']:
         app = UIApplication.sharedApplication()
         params = {'url': info['formats'][-1]['url']}
-        # app.openURL_(nsurl('vlc-x-callback://x-callback-url/stream?' + urlencode(params)))
         app.openURL_(nsurl(params['url']))
     else:
         filepath = ydl.prepare_filename(info)
