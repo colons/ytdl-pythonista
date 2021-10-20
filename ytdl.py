8d2e955613b7ef56267522d746ccad66980bde11
colons <colons@colons.co>
2021-10-20 22:58:45 +0100

    Reduce quality, use yt-dlp, and support single-item playlists.

--- ytdl.py
+++ ytdl.py
@@ -1,7 +1,7 @@
 #!python3
 
 from __future__ import unicode_literals
-import youtube_dl
+from yt_dlp import YoutubeDL
 import appex
 from objc_util import UIApplication, nsurl
 
@@ -17,12 +17,15 @@ print("URL: ", url)
 if not url or not url.startswith("http"):
     url = input("No URL found - enter URL to download: ")
 
-with youtube_dl.YoutubeDL() as ydl:
-    selector = ydl.build_format_selector('best[ext=mp4][height<=?1080]')
+with YoutubeDL() as ydl:
+    selector = ydl.build_format_selector('best[ext=mp4][height<=?720]')
 
     info = ydl.extract_info(url, download=False)
     if info.get('_type') == 'playlist':
-        raise NotImplementedError(f'{url} is a playlist')
+        entries = info['entries']
+        if len(entries) != 1:
+            raise NotImplementedError(f'{url} is a playlist with {len(entries)} entries')
+        info, = entries
 
     selected_format, = selector({'formats': info['formats']})
     video_url = selected_format['url']
